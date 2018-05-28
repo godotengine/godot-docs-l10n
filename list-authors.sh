755096271e52ef834d5108925ed111b4ef2d788d
Rémi Verschelde <rverschelde@gmail.com>
2018-05-28 10:30:16 +0200

    Improve listing of translation authors

--- list-authors.sh
+++ list-authors.sh
@@ -7,7 +7,13 @@ fi
 
 commit=$1
 
+rm -f authors.txt langs.txt
+
 for file in weblate/*.po; do
-  echo -e "\n* $file:\n"
-  git log $commit..HEAD --date=format:"%Y" --format="%aN <%aE>, %ad." $file | grep -v 'anonymous' | sort -u
+  authors=$(git log $commit..HEAD --date=format:"%Y" --format="%aN <%aE>, %ad." $file | grep -v 'anonymous' | sort -u)
+  if [ ! -z "$authors" ]; then
+    echo "$file" >> langs.txt
+    echo -e "\n* $file:\n" >> authors.txt
+    echo "$authors" >> authors.txt
+  fi
 done
