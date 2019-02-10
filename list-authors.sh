2e37a50bc02755e6d7f29764e616746c422571a0
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-02-10 10:28:50 +0100

    Fix warnings reported by ShellCheck and use Bash strict mode
    See http://redsymbol.net/articles/unofficial-bash-strict-mode/.

--- list-authors.sh
+++ list-authors.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
+set -euo pipefail
+IFS=$'\n\t'
+
+if [ -z "$1" ]; then
   echo "Pass a commit hash as argument to list authors between that commit and HEAD."
   exit 1
 fi
@@ -10,16 +13,16 @@ commit=$1
 rm -f authors.txt langs.txt
 
 for file in weblate/*.po; do
-  authors=$(git log $commit..HEAD --date=format:"%Y" --format="%aN <%aE>, %ad." $file | grep -v 'anonymous' | sort -u)
-  if [ ! -z "$authors" ]; then
+  authors=$(git log "$commit"..HEAD --date=format:"%Y" --format="%aN <%aE>, %ad." "$file" | grep -v 'anonymous' | sort -u)
+  if [ -n "$authors" ]; then
     new_authors=""
     while read -r author; do
-      found=$(grep "$author" $file)
+      found=$(grep "$author" "$file")
       if [ -z "$found" ]; then
          new_authors+=$author$'\n'
       fi
     done <<< "$authors"
-    if [ ! -z "$new_authors" ]; then
+    if [ -z "$new_authors" ]; then
       echo "$file" >> langs.txt
       echo -e "\n* $file:\n" >> authors.txt
       echo "$new_authors" >> authors.txt
